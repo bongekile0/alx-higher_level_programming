57a4f4e63aa12ba32e848cc4dac534b861a99c48
bongekile0 <mahlabangane.co.za@gmail.com>
2024-01-08 13:14:14 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 5-no_c.py
 
-
-def no_c(my_string):
-    """Remove all characters c and C from a string."""
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
+    for list in matrix:
+        print(" ".join("{:d}".format(j)for j in list))
